92b26c9833313a3597361b221f47a01ed2bbc203
Mahammad Haneef Gorantla <55850551+haneefnani@users.noreply.github.com>
2022-05-24 10:47:15 +0530

    First Flask App

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+            return "Hello world"
+
+@app.route('/<myname>')
+def newproject(myname):
+            return "Welcome to {}".format(myname)
+if __name__ == '__main__':
+            app.run(debug=True)
+
